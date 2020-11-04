b81c7462142fe1a56fdbc4ce5cf6576b3713eadb
salamanderjones <73806214+salamanderjones@users.noreply.github.com>
2020-11-04 12:54:07 -0600

    Add files via upload

---
+++ video3.py
@@ -0,0 +1,13 @@
+from math import *
+print(-2.95 * (9 + 3))
+
+my_number = 5
+print(str(my_number)) #this turns it into a string. helps when you want to print numbers outside of strings
+print(str(my_number)) + " is my favorite number"
+
+other_number = -10
+print(abs(other_number)) #prints absolute value
+print(pow(4, 6)) #this is 4^6, 4 to the power of 6
+print(max(3,5)) #tells us which number is higher
+print(min(3, 5)) #this tells us the minimum
+print(round(11.23)) #this rounds the number
