70fc3402a622039f0f8da8850fd8e96741345334
salamanderjones <73806214+salamanderjones@users.noreply.github.com>
2020-11-04 12:39:11 -0600

    Add files via upload

---
+++ video2.py
@@ -0,0 +1,11 @@
+print("Giraffe\nAcademy") #puts Academy on the next line
+print("Giraffe\"Academy")
+phrase = "Ginger Snap"
+print(phrase + " is tasty")
+print(phrase.lower())
+print(len(phrase)) #tells us length of the phrase
+print(phrase.isupper()) #asking if string is upper case or nah
+print(phrase[0]) #Tells me first character of phrase. We start with zero.
+print(phrase.index("r")) #Says 5, because that's where G is
+print(phrase.index("Snap")) #tells you where Snap starts
+print(phrase.replace("Ginger", "Holy")) #This replaces ginger with holy
