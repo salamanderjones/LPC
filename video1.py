f204b5b79373f5ab52a8edb1438eaca142537572
salamanderjones <73806214+salamanderjones@users.noreply.github.com>
2020-11-03 20:44:31 -0600

    Add files via upload

---
+++ video1.py
@@ -0,0 +1,7 @@
+character_name = "Perry" #string is Perry, it's plain text
+character_age = "35"
+print("There once was a man named " + character_name + ",")
+print("he was " + character_age + " years old. ")
+character_name = "Sam" #this updates name of variable
+print("He really liked the name  " + character_name + ",")
+print("but didn't like being " + character_age + ".")
